1cd717c3d0a89f034f12a3defb5aea3cc3c0f6b5
FranzB445 <franzbatiancila14@gmail.com>
2024-11-13 10:06:53 +0800

    Add files via upload

---
+++ LABEXERSS.java
@@ -0,0 +1,36 @@
+
+package labexerss;
+
+
+public class LABEXERSS {
+
+    
+    public static void main(String[] args) {
+        
+        int iNumber = 0;
+        if (iNumber >=0){    
+           
+        
+        System.out.println("First String");
+        
+          }
+        
+        
+         if (iNumber ==0){
+        
+         System.out.println("Second String");   
+         
+         }
+         
+         else{
+             System.out.println("Thrid string");
+                  
+         }
+         
+        
+        
+    
+        
+    }
+    
+}
